678ba351be699519ccf17013f6ba1401ad238730
siamak a.m.o <edu.siamak@gmail.com>
2023-11-24 22:39:39 +0330

    add log functions

--- go_server/server/serlog/logging.go
+++ go_server/server/serlog/logging.go
@@ -2,6 +2,7 @@ package serlog
 
 import (
 	"fmt"
+	"os"
 	"server/server/config"
 )
 
@@ -20,25 +21,62 @@ type Log struct {
 }
 
 func level2str(level Lev) string {
-	switch level{
-		case Debug: return "[Debug] "
-		case Info: return "[INFO] "
-		case Warning: return "[Warning] "
-		case Error: return "ERROR -- "
-		case Panic: return "*PANIC* -- "
+	switch level {
+	case Debug:
+		return "[Debug] "
+	case Info:
+		return "[INFO] "
+	case Warning:
+		return "[Warning] "
+	case Error:
+		return "ERROR -- "
+	case Panic:
+		return "*PANIC* -- "
 	}
 	return ""
 }
 
-func New(cfg config.Config) *Log{
+func New(cfg config.Config) *Log {
 	return &Log{
 		LogLevel: cfg.Log.LogLevel,
 	}
 }
 
-func (l Log) Logf(level Lev, format string, args ...any){
+func (l Log) logf(level Lev, def func(), format string, args ...any) {
+	defer def()
+
 	if level >= Lev(l.LogLevel) {
 		fmt.Print(level2str(level))
 		fmt.Printf(format, args...)
+	}
+}
+
+func nop() {}
+func exit() {
+	os.Exit(1)
+}
+
+func (l Log) Debugf(format string, args ...any) {
+	l.logf(Debug, nop, format, args...)
+}
+func (l Log) Infof(format string, args ...any) {
+	l.logf(Info, nop, format, args...)
+}
+func (l Log) Warnf(format string, args ...any) {
+	l.logf(Warning, nop, format, args...)
+}
+
+func (l Log) Errorf(format string, fun func(), args ...any) {
+	if fun != nil {
+		l.logf(Error, fun, format, args...)
+	} else {
+		l.logf(Error, exit, format, args...)
+	}
+}
+func (l Log) Panicf(format string, fun func(), args ...any) {
+	if fun != nil {
+		l.logf(Panic, fun, format, args...)
+	} else {
+		l.logf(Panic, exit, format, args...)
 	}
 }
