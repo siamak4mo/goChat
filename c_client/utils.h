e4274b03974b6d6878ac08293140735462a74090
siamak a.m.o <edu.siamak@gmail.com>
2023-12-08 16:37:38 +0330

    introduce two macros for iterating wchar pointer

--- c_client/utils.h
+++ c_client/utils.h
@@ -15,4 +15,18 @@ typedef bool lock_t;
   _LOCK(x);              } while (0)
 
 
+// for loop for wchar_t
+// @_ptr should be a char pointer
+#define WCHAR4(_ptr, X)                         \
+  for (_ptr = (char*) X;                        \
+       !(_ptr[0] == 0 && _ptr[1] == 0 &&        \
+         _ptr[2] == 0 && _ptr[3] == 0); _ptr += 4)
+
+// from char pointer @X to wchar_t @_ptr
+// @X should be a char pointer and @_ptr wchar_t
+#define WCHAR2(_wch, X)                         \
+  for (char *__p = X;                           \
+       ({(_wch) = *__p; *__p != '\0';}); __p++)
+
+
 #endif
