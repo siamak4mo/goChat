ef38cfcbed76ba763e7883b2920083cf2e9f3514
siamak a.m.o <edu.siamak@gmail.com>
2024-02-19 19:19:49 +0330

    change using global vars in server_main.go to using main_controller

--- go_server/admin_server.go
+++ go_server/admin_server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	server "server/chat_server"
+	"server/chat_server/config"
 	"server/chat_server/serlog"
 )
 
@@ -17,6 +18,7 @@ type AdminServer struct {
 	Handlers   map[string]AdminHandler `json:"routes"`
 	ChatServer *server.Server          `json:"-"`
 	Loger      *serlog.Log             `json:"-"`
+	Conf       *config.Config          `json:"-"`
 }
 
 func (s *AdminServer) Server() error {
@@ -24,71 +26,88 @@ func (s *AdminServer) Server() error {
 		http.HandleFunc(p, fun.HandlerFunc)
 	}
 
-	admin_s.Loger.Pprintf("Listening on %s\n", conf.Admin.Addr)
-	err := http.ListenAndServe(conf.Admin.Addr, nil)
+	s.Loger.Pprintf("Listening on %s\n", s.Conf.Admin.Addr)
+	err := http.ListenAndServe(s.Conf.Admin.Addr, nil)
 	if err != nil {
-		println("admin server -- could not listen on " + conf.Admin.Addr)
+		println("admin server -- could not listen on " + s.Conf.Admin.Addr)
 		return err
 	}
 
 	return nil
 }
 
-func NewAdminServer(chat_server *server.Server) *AdminServer {
+func NewAdminServer(controller *main_controller) *AdminServer {
 	h := make(map[string]AdminHandler)
 
+	s := &AdminServer{
+		Handlers:   h,
+		ChatServer: controller.chat_s,
+		Loger:      serlog.New(*controller.config, "Admin Server"),
+		Conf:       controller.config,
+	}
+
 	h["/"] = AdminHandler{
-		HandlerFunc: root,
-		Info:        "root page",
-		Method:      "GET - no param",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.root(w, r)
+		},
+		Info:   "root page",
+		Method: "GET - no param",
 	}
 	h["/register"] = AdminHandler{
-		HandlerFunc: reg_new_user,
-		Info:        "register new user",
-		Method:      "POST - username to sign up",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.reg_new_user(w, r)
+		},
+		Info:   "register new user",
+		Method: "POST - username to sign up",
 	}
 
 	h["/chats/stat"] = AdminHandler{
-		HandlerFunc: chat_stat,
-		Info:        "statistics of chats",
-		Method:      "GET - no param",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.chat_stat(w, r)
+		},
+		Info:   "statistics of chats",
+		Method: "GET - no param",
 	}
 	h["/chat/users"] = AdminHandler{
-		HandlerFunc: chat_users,
-		Info:        "show users of a chat room",
-		Method:      "GET - URL params: ?chat=key (16 byte chat key)",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.chat_users(w, r)
+		},
+		Info:   "show users of a chat room",
+		Method: "GET - URL params: ?chat=key (16 byte chat key)",
 	}
 	h["/chat/add"] = AdminHandler{
-		HandlerFunc: chat_add,
-		Info:        "make a new chat room",
-		Method:      "POST - json req: {\"name\": \"chat name\", \"banner\": \"chat message of the day\"}",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.chat_add(w, r)
+		},
+		Info:   "make a new chat room",
+		Method: "POST - json req: {\"name\": \"chat name\", \"banner\": \"chat message of the day\"}",
 	}
 	h["/chat/remove"] = AdminHandler{
-		HandlerFunc: chat_remove,
-		Info:        "remove a chat room",
-		Method:      "POST - json req: {\"chat key\": \"chat key (16 byte)\"}",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.chat_remove(w, r)
+		},
+		Info:   "remove a chat room",
+		Method: "POST - json req: {\"chat key\": \"chat key (16 byte)\"}",
 	}
 	h["/users/stat"] = AdminHandler{
-		HandlerFunc: user_stat,
-		Info:        "show logged in users",
-		Method:      "GET - no param",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.user_stat(w, r)
+		},
+		Info:   "show logged in users",
+		Method: "GET - no param",
 	}
 	h["/config/lookup"] = AdminHandler{
-		HandlerFunc: config_lookup,
-		Info:        "show the current server configuration",
-		Method:      "GET - no param",
+		HandlerFunc: func(w http.ResponseWriter, r *http.Request) {
+			s.config_lookup(w, r)
+		},
+		Info:   "show the current server configuration",
+		Method: "GET - no param",
 	}
 
-	admin_s = &AdminServer{
-		Handlers:   h,
-		ChatServer: chat_server,
-		Loger:      serlog.New(*conf, "Admin Server"),
-	}
-
-	return admin_s
+	return s
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) root(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
 		res := make(map[string]interface{})
@@ -97,7 +116,7 @@ func root(w http.ResponseWriter, r *http.Request) {
 			Addr   string `json:"address"`
 			Status string `json:"status"`
 		}{
-			Addr:   chat_s.Conf.Server.Addr,
+			Addr:   s.ChatServer.Conf.Server.Addr,
 			Status: "OK",
 		}
 		res["admin server"] = struct {
@@ -106,22 +125,22 @@ func root(w http.ResponseWriter, r *http.Request) {
 			Addr     string                  `json:"address"`
 		}{
 			Name:     "Admin Server",
-			Addr:     conf.Admin.Addr,
-			Handlers: admin_s.Handlers,
+			Addr:     s.Conf.Admin.Addr,
+			Handlers: s.Handlers,
 		}
 		resp, err := json.Marshal(res)
 		if err != nil {
-			admin_s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
+			s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
 		}
 		w.Write(resp)
 	}
 }
-func user_stat(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) user_stat(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
 		res := make(map[string]interface{})
 
-		for addr, lp := range chat_s.Clients {
+		for addr, lp := range s.ChatServer.Clients {
 			res[lp.User.Username] = struct {
 				Addr     string `json:"address"`
 				Token    string `json:"token"`
@@ -131,33 +150,33 @@ func user_stat(w http.ResponseWriter, r *http.Request) {
 				Addr:     addr,
 				Token:    lp.Payload,
 				ChatKey:  lp.User.ChatKey,
-				ChatName: chat_s.ChatName(*lp),
+				ChatName: s.ChatServer.ChatName(*lp),
 			}
 		}
 		resp, err := json.Marshal(res)
 		if err != nil {
-			admin_s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
+			s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
 		}
 		w.Write(resp)
 	}
 }
-func config_lookup(w http.ResponseWriter, r *http.Request) {
-	admin_s.Loger.Warnf("Config Lookup Access\n")
+func (s *AdminServer) config_lookup(w http.ResponseWriter, r *http.Request) {
+	s.Loger.Warnf("Config Lookup Access\n")
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
-		resp, err := json.Marshal(chat_s.Conf)
+		resp, err := json.Marshal(s.Conf)
 		if err != nil {
-			admin_s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
+			s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
 		}
 		w.Write(resp)
 	}
 }
-func chat_stat(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) chat_stat(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
 		res := make(map[string]interface{})
 
-		for k, chat := range chat_s.Chats {
+		for k, chat := range s.ChatServer.Chats {
 			res[k] = struct {
 				Name  string `json:"name"`
 				MOTD  string `json:"banner"`
@@ -170,32 +189,32 @@ func chat_stat(w http.ResponseWriter, r *http.Request) {
 		}
 		resp, err := json.Marshal(res)
 		if err != nil {
-			admin_s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
+			s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
 		}
 		w.Write(resp)
 	}
 }
-func chat_users(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) chat_users(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json")
 		res := make(map[int]string)
 		chat := r.URL.Query().Get("chat")
 
-		if len(chat) == 16 && chat_s.Chats[chat] != nil {
+		if len(chat) == 16 && s.ChatServer.Chats[chat] != nil {
 			idx := 0
-			for lp := range chat_s.Chats[chat].Members {
+			for lp := range s.ChatServer.Chats[chat].Members {
 				res[idx] = lp.User.Username
 				idx += 1
 			}
 		}
 		resp, err := json.Marshal(res)
 		if err != nil {
-			admin_s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
+			s.Loger.Warnf("json.Marshal error: %s\n", err.Error())
 		}
 		w.Write(resp)
 	}
 }
-func chat_add(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) chat_add(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		dec := json.NewDecoder(r.Body)
 		data := struct {
@@ -205,14 +224,14 @@ func chat_add(w http.ResponseWriter, r *http.Request) {
 		err := dec.Decode(&data)
 
 		if err == nil {
-			if !chat_s.HasChat(data.Name) {
-				chat_s.AddNewChat(data.Name, data.MOTD)
+			if !s.ChatServer.HasChat(data.Name) {
+				s.ChatServer.AddNewChat(data.Name, data.MOTD)
 				w.Write([]byte("Added\n"))
 			}
 		}
 	}
 }
-func reg_new_user(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) reg_new_user(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		dec := json.NewDecoder(r.Body)
 		data := struct {
@@ -221,14 +240,14 @@ func reg_new_user(w http.ResponseWriter, r *http.Request) {
 		err := dec.Decode(&data)
 
 		if err == nil {
-			user_token := chat_s.RegisterUser(data.Name)
+			user_token := s.ChatServer.RegisterUser(data.Name)
 			w.Write([]byte("Token: " + user_token + "\n"))
 		} else {
 			w.Write([]byte("ops\n" + err.Error()))
 		}
 	}
 }
-func chat_remove(w http.ResponseWriter, r *http.Request) {
+func (s *AdminServer) chat_remove(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		dec := json.NewDecoder(r.Body)
 		data := struct {
@@ -237,8 +256,8 @@ func chat_remove(w http.ResponseWriter, r *http.Request) {
 		err := dec.Decode(&data)
 
 		if err == nil {
-			if chat_s.HasChatKey(data.Key) {
-				chat_s.RemoveChat(data.Key)
+			if s.ChatServer.HasChatKey(data.Key) {
+				s.ChatServer.RemoveChat(data.Key)
 				w.Write([]byte("Removed\n"))
 			}
 		}
