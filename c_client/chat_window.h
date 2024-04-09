153138118b5377c716b521e0b3259c424c9c85bc
siamak a.m.o <edu.siamak@gmail.com>
2024-04-09 15:30:00 +0330

    add comment

--- c_client/chat_window.h
+++ c_client/chat_window.h
@@ -14,6 +14,8 @@ typedef struct {
 
 #define ST_CUR(win, ry, rx) getyx (win, ry, rx);
 #define LD_CUR(win, ry, rx) wmove (win, ry, rx);
+/* use this macro to run procedures that might involve */
+/* moving the cursor, pass __DO__ like {... f(x); ...} */
 #define SAFE_CW(win, __DO__) do {                       \
     int __y, __x;                                       \
     ST_CUR(win, __y, __x);                              \
@@ -24,15 +26,21 @@ typedef struct {
 static const wchar_t ME[] = {'(', '*', ')', '|', ' ', '\0'};
 #define cw_write_my_mess(cw, buf) cw_vawrite (cw, 2, ME, buf);
 
+
+/* create and initialize chat window */
+chatw mk_chatw(int, int, bool);
 void init_chat_window(chatw *, int, int);
+/* set window name (displayed on the top-right corner) */
 void cw_set_name(chatw *cw, const char *name);
+/* clear contents */
 void cw_clear(chatw *);
+/* write text to chat window functions */
 void cw_write(chatw *, const wchar_t *);
 void cw_write_char(chatw *, const char *);
 void cw_write_char_mess(chatw *, const char *);
 void cw_vawrite(chatw *, int, ...);
 void cw_vawrite_char(chatw *, int, ...);
-chatw mk_chatw(int, int, bool);
+/* read from chat window functions */
 int cw_read(chatw *, wchar_t *, int);
 void cw_end (chatw *);
 #endif
